40b99d987bd255f7d44e7f5fc55922e126132a56
logan11116 <romachicu@gmail.com>
2016-05-05 11:30:33 +0300

    Laboratorul nr.5 Batalia Spatiala

---
+++ lab 5/Windows Phone 8/DefaultPackageName/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+using DefaultPackageName.Resources;
+
+namespace DefaultPackageName
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
